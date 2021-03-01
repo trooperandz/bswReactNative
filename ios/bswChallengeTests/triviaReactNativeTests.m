c437ce8b63a0e1947e505083da86000585585c5c
Matthew Holland <mtholland10@gmail.com>
2021-02-28 19:36:04 -0700

    Update readme

--- ios/bswChallengeTests/triviaReactNativeTests.m
+++ ios/bswChallengeTests/triviaReactNativeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface triviaReactNativeTests : XCTestCase
+@interface bswChallengeTests : XCTestCase
 
 @end
 
-@implementation triviaReactNativeTests
+@implementation bswChallengeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
